acf13ca203d5d7a934acc9e562c4019e0ef6edef
juhwang <juhong4930@naver.com>
2023-01-18 15:10:52 +0900

    feat: 소셜 로그인 구글 추가

--- src/main/java/com/woojoofolio/project/springboot/web/IndexController.java
+++ src/main/java/com/woojoofolio/project/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.woojoofolio.project.springboot.web;
 
+import com.woojoofolio.project.springboot.config.auth.dto.SessionUser;
 import com.woojoofolio.project.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,21 +8,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("name", user.getName());
+        }
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String postsSave() {
+    public String postsSave(Model model) {
+
+        model.addAttribute("posts", postsService.findAllDesc());
         return "posts-save";
     }
 
