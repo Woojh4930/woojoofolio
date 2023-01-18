9cf3fdafbe3f675a82017d1f93dfc4be789af60e
juhwang <juhong4930@naver.com>
2023-01-18 16:46:22 +0900

    test: 메인페이지 로딩 테스트

--- src/test/java/com/woojoofolio/project/springboot/web/IndexControllerTest.java
+++ src/test/java/com/woojoofolio/project/springboot/web/IndexControllerTest.java
@@ -20,7 +20,7 @@ public class IndexControllerTest {
     @Test
     public void 메인페이지_로딩() {
         //when
-        String body = this.restTemplate.getForObject("/",String.class);
+        String body = this.restTemplate.getForObject("/", String.class);
 
         //then
         assertThat(body).contains("WooJooFolio");
