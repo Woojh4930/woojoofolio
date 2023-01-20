bd57e6f756f0b1d0346aec93f8872ff6dbf0398d
juhwang <juhong4930@naver.com>
2023-01-20 12:18:18 +0900

    chore: 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,27 +3,13 @@
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=woojoofolio
 
-cd $REPOSITORY/$PROJECT_NAME/
-
-echo "> Git Pull"
-
-git pull
-
-echo "> 프로젝트 Build 시작"
-
-./gradlew build
-
-echo "> step1 디렉토리로 이동"
-
-cd $REPOSITORY
-
 echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+CURRENT_PID=$(pgrep -fl woojoofolio | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
