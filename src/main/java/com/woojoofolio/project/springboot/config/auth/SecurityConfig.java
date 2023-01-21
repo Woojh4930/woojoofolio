bbb2ab21b510c1a0d70e0143db7e449ac116d691
juhwang <juhong4930@naver.com>
2023-01-21 15:16:29 +0900

    test: 포트를 정할 프로필 API 테스트

--- src/main/java/com/woojoofolio/project/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/woojoofolio/project/springboot/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable() // h2-console 화면을 사용하기 위한 장치
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
