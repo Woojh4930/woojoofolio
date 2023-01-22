c491a7cdd7b0cedc911b3755eafc7d8bb56383c2
juhwang <juhong4930@naver.com>
2023-01-22 19:52:33 +0900

    refactor: 구글 로그인 버튼 수정

---
+++ src/main/resources/static/js/app/oauth.js
@@ -0,0 +1,14 @@
+function onHover()
+{
+    $("#btn-google").attr('src', '/images/google/btn_google_signin_dark_focus_web.png');
+}
+
+function offHover()
+{
+    $("#btn-google").attr('src', '/images/google/btn_google_signin_dark_normal_web.png');
+}
+
+function onClick()
+{
+    $("#btn-google").attr('src', '/images/google/btn_google_signin_dark_pressed_web.png');
+}
