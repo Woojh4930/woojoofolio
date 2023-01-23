4a4ab7169cb52b2eec4ac2a6f4ce0df7eb50a2a2
juhwang <juhong4930@naver.com>
2023-01-23 15:24:39 +0900

    refactor: 로그인 버튼 및 전체적인 UI 수정

--- src/main/resources/static/js/app/oauth.js
+++ src/main/resources/static/js/app/oauth.js
@@ -1,14 +1,52 @@
-function onHover()
-{
-    $("#btn-google").attr('src', '/images/google/btn_google_signin_dark_focus_web.png');
+var oauth = {
+    init: function () {
+        let _this = this;
+
+        $('#btn-google').on("click", function () {
+            _this.move("google");
+        })
+
+        $('#btn-google').on("mouseover", function () {
+            _this.over();
+        })
+
+        $('#btn-google').on("mouseout", function () {
+            _this.out();
+        })
+
+        $('#btn-naver').on("click", function () {
+            _this.move("naver");
+        })
+
+        $('#btn-naver').on("mouseover", function () {
+            _this.over_naver();
+        })
+
+        $('#btn-naver').on("mouseout", function () {
+            _this.out_naver();
+        })
+    },
+
+    move: function (address) {
+        $('#btn-google').attr('src', '/images/google/btn_google_dark_pressed_ios.svg');
+        location.href = "/oauth2/authorization/"+address;
+    },
+
+    over: function () {
+        $('#btn-google').attr('src', '/images/google/btn_google_dark_focus_ios.svg');
+    },
+
+    over_naver: function () {
+        $('#btn-naver').attr('class', 'btn-oauth naver-over size-40');
+    },
+
+    out: function () {
+        $('#btn-google').attr('src', '/images/google/btn_google_dark_normal_ios.svg');
+    },
+
+    out_naver: function () {
+        $('#btn-naver').attr('class', 'btn-oauth size-40');
+    }
 }
 
-function offHover()
-{
-    $("#btn-google").attr('src', '/images/google/btn_google_signin_dark_normal_web.png');
-}
-
-function onClick()
-{
-    $("#btn-google").attr('src', '/images/google/btn_google_signin_dark_pressed_web.png');
-}
+oauth.init();
