592c3a61644bdad973572d860ccb067d5d6525b4
juhwang <juhong4930@naver.com>
2023-01-25 01:05:40 +0900

    feat: 세션에 저장할 유저 정보에 역할 추가

--- src/main/java/com/woojoofolio/project/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/woojoofolio/project/springboot/config/auth/dto/SessionUser.java
@@ -1,5 +1,6 @@
 package com.woojoofolio.project.springboot.config.auth.dto;
 
+import com.woojoofolio.project.springboot.domain.user.Role;
 import com.woojoofolio.project.springboot.domain.user.User;
 import lombok.Getter;
 
@@ -11,10 +12,12 @@ public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
+    private boolean authority;
 
     public SessionUser(User user) {
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
+        this.authority = Role.USER.equals(user.getRole());
     }
 }
