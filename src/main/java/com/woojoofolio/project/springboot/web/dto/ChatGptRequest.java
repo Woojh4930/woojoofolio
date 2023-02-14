9a5d7410ab2dbb3058f722f4f79d5750e69d9d00
juhwang <juhong4930@naver.com>
2023-02-14 17:31:17 +0900

    hotfix: 챗봇기능 일시 롤백

--- src/main/java/com/woojoofolio/project/springboot/web/dto/ChatGptRequest.java
+++ src/main/java/com/woojoofolio/project/springboot/web/dto/ChatGptRequest.java
@@ -3,6 +3,8 @@ package com.woojoofolio.project.springboot.web.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 public class ChatGptRequest {
@@ -10,9 +12,12 @@ public class ChatGptRequest {
     private final String model = "text-davinci-003";
     private String prompt;
     private final Integer max_tokens = 1024;
-    private final double temperature = 0.7;
+    private final double temperature = 0.9;
+    private final double presence_penalty = 0.6;
+    private final List<String> stop = List.of(" Human:", " AI:");
+
 
     public void setPrompt(String prompt) {
-        this.prompt = prompt;
+        this.prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\\n\\nHuman: Hello, who are you?\\nAI: I am an AI created by OpenAI. How can I help you today?\\nHuman: " + prompt;
     }
 }
